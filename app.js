8108c429505e0f6364e4814dadce8347e759c3b4
Maha Bradai <bradaimaha96@gmail.com>
2026-03-15 23:38:34 +0100

    update project

--- app.js
+++ app.js
@@ -1,49 +1,56 @@
-// Importations
 const dotenv = require('dotenv');
-dotenv.config();
 
+dotenv.config();
+console.log("ENV TEST =>", process.env.DATABASE);
+console.log("PORT TEST =>", process.env.PORT);
 const express = require('express');
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const cors = require('cors');
 const path = require('path');
 
 const app = express();
-
-// =======================
-// Middlewares
-// =======================
-
-app.use(cors());
-app.use(express.json());
-
-// =======================
-// Connexion MongoDB
-// =======================
-
-mongoose.connect(process.env.DATABASE)
-.then(() => {
-    console.log("Database Successfully Connected");
-})
-.catch(err => {
-    console.error("Unable to connect to database:", err);
-});
-
-// =======================
-// Routes API
-// =======================
 
 const categorieRouter = require("./routes/categorie.route");
 const scategorieRouter = require("./routes/scategorie.route");
 const articleRouter = require("./routes/article.route");
 const userRouter = require("./routes/user.route");
 
+// config dotenv
+dotenv.config();
+
+// cors
+app.use(cors());
+
+// BodyParser
+app.use(express.json());
+
+// connexion base de données
+mongoose.connect(process.env.DATABASE)
+.then(() => {
+    console.log("DataBase Successfully Connected");
+})
+.catch(err => {
+    console.log("Unable to connect to database", err);
+    process.exit();
+});
+
+
+// routes API
 app.use('/api/users', userRouter);
 app.use('/api/categories', categorieRouter);
 app.use('/api/scategories', scategorieRouter);
 app.use('/api/articles', articleRouter);
 
-// =======================
-// Export ONLY (IMPORTANT)
-// =======================
+//dist reactjs
+app.use(express.static(path.join(__dirname, './client/build'))); // Route pour
+app.get('/{*any}', (req, res) => { res.sendFile(path.join(__dirname,'./client/build/index.html')); });
+
+
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port ${process.env.PORT}`);
+});
 
 module.exports = app;
